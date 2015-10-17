ba4a2142f3634e9473878a435c84effc9af656ed
Raghu Betina <rbetina@users.noreply.github.com>
2015-10-17 10:02:16 -0500

    ROCK: Add logic to action

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,6 +1,14 @@
 class GameController < ApplicationController
   def user_plays_rock
+    @computer_move = ["rock", "paper", "scissors"].sample
 
+    if @computer_move == "rock"
+      @outcome = "tied"
+    elsif @computer_move == "paper"
+      @outcome = "lost"
+    elsif @computer_move == "scissors"
+      @outcome = "won"
+    end
 
     render("user_plays_rock.html.erb")
   end
