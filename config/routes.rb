7e0b7179568dc7ad39d66c4677cf3b5e3fe1457d
Raghu Betina <rbetina@users.noreply.github.com>
2015-10-17 10:39:31 -0500

    Add root URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "game", :action => "user_plays_rock" })
+
   get("/rock", { :controller => "game", :action => "user_plays_rock" })
 
   get("/paper", { :controller => "game", :action => "user_plays_paper" })
